816e0aab0d522ad27ae275dc80086b410f8c846b
zvadym <zvadym@Olgas-MacBook-Pro.local>
2019-10-15 18:35:06 +0300

    UserInfo + logout function

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="user">
+    <q-btn round color="indigo" glossy icon="wc" />
+    <q-menu transition-show="flip-right" transition-hide="flip-left">
+      <q-list style="min-width: 120px">
+        <q-item>
+          <q-item-section>{{ user.name }}</q-item-section>
+        </q-item>
+        <q-item>
+          <q-item-section>{{ user.email }}</q-item-section>
+        </q-item>
+        <q-separator />
+        <q-item clickable @click="signOut">
+          <q-item-section>Sign Out</q-item-section>
+        </q-item>
+      </q-list>
+    </q-menu>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import firebase from 'firebase/app'
+
+export default {
+  computed: {
+    ...mapGetters(['user'])
+  },
+  methods: {
+    ...mapActions(['setUser']),
+    signOut() {
+      firebase.auth().signOut()
+      this.setUser(null)
+    }
+  }
+}
+</script>
